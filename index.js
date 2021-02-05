8915c35d8aaee18023eb678ca68c1465feb969fa
Arun <arun@productiv.ai>
2021-02-04 23:12:20 -0800

    CR

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 8081;
 var express = require('express');
+const path = require('path')
 var app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
@@ -17,9 +18,14 @@ const _ = require('lodash');
 // });
 // app.use(express.static(__dirname + '/public'));
 //
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
 const server = new SequenceServer();
 server.start(io);
 http.listen(PORT, function() {
